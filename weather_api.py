672b64f347866c9348b9527c74c9fb0ee5684255
Дмитрий <chernovdm@list.ru>
2024-12-20 01:47:23 +0300

    first_commit

--- weather_api.py
+++ weather_api.py
@@ -22,34 +22,46 @@ DIRECTION_TRANSFORM = {
     'c': 'штиль',
 }
 def current_weather_api(city):
-  ...
+    token = '5d591e2cb44a4539b59115302231112'
+    url = f'https://api.weatherapi.com/v1/current.json?key={token}&q={city}'
+    response = requests.get(url)
+    print(response.text)
+    data = response.json()
+    s = f'Город: {data["location"]["name"]}\n'\
+        f'Страна: {data["location"]["country"]}\n'\
+        f'Температура: {data["current"]["temp_c"]}\n'\
+        f'Ветер: {data["current"]["wind_kph"]}\n'\
+        f'Ощущается: {data["current"]["feelslike_c"]}\n'\
+        f'Время обновления: {data["current"]["last_updated"]}'
+    print(s)
 
-# def current_weather(lat, lon):
-#     """
-#     Описание функции, входных и выходных переменных
-#     """
-#     token = 'Ваш токен'  # Вставить ваш токен
-#     url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"  # Если вдруг используете тариф «Погода на вашем сайте»
-#     # то вместо forecast используйте informers. url = f"https://api.weather.yandex.ru/v2/informers?lat={lat}&lon={lon}"
-#     headers = {"X-Yandex-API-Key": f"{token}"}
-#     response = requests.get(url, headers=headers)
-#     data = response.json()
-#
-#     # Данная реализация приведена для тарифа «Тестовый», если у вас Тариф «Погода на вашем сайте», то закомментируйте пару строк указанных ниже
-#     result = {
-#         'city': data['geo_object']['locality']['name'],  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
-#         'time': datetime.fromtimestamp(data['fact']['uptime']).strftime("%H:%M"),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
-#         'temp': 'реализация',  # TODO Реализовать вычисление температуры из данных полученных от API
-#         'feels_like_temp': 'реализация',  # TODO Реализовать вычисление ощущаемой температуры из данных полученных от API
-#         'pressure': 'реализация',  # TODO Реализовать вычисление давления из данных полученных от API
-#         'humidity': 'реализация',  # TODO Реализовать вычисление влажности из данных полученных от API
-#         'wind_speed': 'реализация',  # TODO Реализовать вычисление скорости ветра из данных полученных от API
-#         'wind_gust': 'реализация',  # TODO Реализовать вычисление скорости порывов ветка из данных полученных от API
-#         'wind_dir': DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
-#     }
-#     return result
+def current_weather(lat, lon):
+    """
+    Описание функции, входных и выходных переменных
+    """
+    token = 'fc49970d-6550-464c-b92b-ebe80078b1d3'  # Вставить ваш токен
+    url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"  # Если вдруг используете тариф «Погода на вашем сайте»
+    # то вместо forecast используйте informers. url = f"https://api.weather.yandex.ru/v2/informers?lat={lat}&lon={lon}"
+    headers = {"X-Yandex-API-Key": f"{token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    # print(response.json())
+
+    # Данная реализация приведена для тарифа «Тестовый», если у вас Тариф «Погода на вашем сайте», то закомментируйте пару строк указанных ниже
+    result = f'city: {data["info"]["tzinfo"]["name"]}\n'\
+             f'time: {datetime.fromtimestamp(data["fact"]["uptime"]).strftime("%H:%M")}\n'\
+             f'temp: {data["fact"]["temp"]}\n'\
+             f'feels_like_temp: {data["fact"]["feels_like"]}\n'\
+             f'pressure: {data["fact"]["pressure_mm"]}\n'\
+             f'humidity: {data["fact"]["humidity"]}\n'\
+             f'wind_speed: {data["fact"]["wind_speed"]}\n'\
+             f'wind_gust: {data["fact"]["wind_gust"]}\n'\
+             #f'wind_dir: DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
+
+    return result
 
 
 if __name__ == "__main__":
     ...
-    # print(current_weather(59.93, 30.31))  # Проверка работы для координат Санкт-Петербурга
+    print(current_weather(59.93, 30.31))  # Проверка работы для координат Санкт-Петербурга
+    # current_weather_api('Moscow')
